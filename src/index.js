03e8fc987a8b1b979f8dac10df1a405100d9acc7
Misly <60405462+Misly16@users.noreply.github.com>
2021-08-31 19:59:53 +0000

    request and getUser function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch');
+const pkg = require('../package.json');
+
 /**
  * Node.JS module for interacting with Playcap's API.
  * @example
@@ -15,6 +18,33 @@ class Api {
     this.apiToken = apiToken;
 
     if (!apiToken) throw new Error('[PLAYCAP.APP] No apiToken was specified.');
+  }
+
+  /**
+   * @private
+   * @param {string} path the path that request will be sent to
+   * @param {string} apiToken the token which the client will use to interact with the api
+   * @param {string} method the method in which the request will be sent
+   * @param {object} body the request body
+   */
+  async _request(path, apiToken, method, body) {
+    const baseUrl = 'https://api.playcap.app/api/v1/';
+    return await fetch(`${baseUrl}${path}`, {
+      method: method,
+      body: body ? '' : JSON.stringify(body),
+      headers: {'Content-Type': 'application/json', 'User-Agent': `playcap.app-js v${pkg.version}`},
+    }).then((body) => {
+      if (!body) throw Error('[PLAYCAP.APP] Response did not return a body.');
+      return body;
+    });
+  }
+  /**
+   *
+   * @param {string} userId the id of the user to fetch
+   * @return {object} body
+   */
+  getUser(userId) {
+    return this._request(`users/${userId}`, this.apiToken, 'GET');
   }
 }
 
