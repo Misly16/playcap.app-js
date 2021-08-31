3a66809315c2038512b8e3284593916e6e084caa
Misly <60405462+Misly16@users.noreply.github.com>
2021-08-31 14:47:10 +0000

    jsdoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+/**
+ * Node.JS module for interacting with Playcap's API.
+ * @example
+ * ```js
+ * const Playcap = require('playcap.app');
+ * const playcap = new Playcap('apiKey');
+ * ```
+ */
 class Api {
+/**
+*
+* @param {string} apiToken the token which the client will use to interact with the api
+*/
+  constructor(apiToken) {
+    this.apiToken = apiToken;
 
-
+    if (!apiToken) throw new Error('[PLAYCAP.APP] No apiToken was specified.');
+  }
 }
 
 module.exports = Api;
